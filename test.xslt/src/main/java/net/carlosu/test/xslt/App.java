a5b347e097dd3d5e45bb4f83345251074881f4f2
carlos <carlosurquizad@gmail.com>
2014-11-18 16:29:52 -0500

    18/11/2014

---
+++ test.xslt/src/main/java/net/carlosu/test/xslt/App.java
@@ -0,0 +1,40 @@
+package net.carlosu.test.xslt;
+
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try {
+    		InputStream input = ClassLoader.getSystemResourceAsStream("styler.xsl");
+    		
+    		StringBuilder sb = new StringBuilder();
+    		sb.append("<?xml version=\"1.0\"?>");
+    		sb.append("<hello-world><greeter>An XSLT Programmer</greeter><greeting>Hello, World!</greeting></hello-world>");
+    		StringReader reader = new StringReader(sb.toString());
+
+    	    StringWriter writer = new StringWriter();
+    	    TransformerFactory tFactory = TransformerFactory.newInstance();
+    	    Transformer transformer = tFactory.newTransformer(new StreamSource(input));
+
+    	    transformer.transform(new StreamSource(reader), new StreamResult(writer));
+
+    	    String result = writer.toString();
+    	    System.out.println(result);
+    	} catch (Exception e) {
+    	    e.printStackTrace();
+    	}
+    }
+}
