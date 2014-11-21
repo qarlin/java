aa11eaa9422ef9c0355263ce794012293bf84d30
carlos <carlosurquizad@gmail.com>
2014-11-21 12:53:03 -0500

    21/11/2014

---
+++ guava/src/main/java/net/carlosu/guava/Product.java
@@ -0,0 +1,27 @@
+package net.carlosu.guava;
+
+public class Product {
+	private Integer id;
+	private String description;
+	private String category;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+}
---
+++ guava/src/test/java/net/carlosu/guava/CollectionsTest.java
@@ -0,0 +1,126 @@
+package net.carlosu.guava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimaps;
+
+public class CollectionsTest {
+	private List<Product> prods = new ArrayList<Product>();
+
+	@Before
+	public void setUp() throws Exception {
+		Product p1 = new Product();
+		p1.setId(1);
+		p1.setDescription("desc 1");
+		p1.setCategory("cat1");
+		prods.add(p1);
+
+		Product p2 = new Product();
+		p2.setId(2);
+		p2.setDescription("desc 2");
+		p2.setCategory("cat1");
+		prods.add(p2);
+
+		Product p3 = new Product();
+		p3.setId(3);
+		p3.setDescription("desc 3");
+		p3.setCategory("cat2");
+		prods.add(p3);
+	}
+
+	@Test
+	public void transformCollectionTest() {
+		List<Integer> ids = Lists.transform(prods,
+				new Function<Product, Integer>() {
+					public Integer apply(Product p) {
+						return p.getId();
+					}
+				});
+
+		Assert.assertEquals(3, ids.size());
+	}
+
+	@Test
+	public void indexCollectionTest() {
+		Map<Integer, Product> prodById = Maps.uniqueIndex(prods,
+				new Function<Product, Integer>() {
+					public Integer apply(Product p) {
+						return p.getId();
+					}
+				});
+
+		Assert.assertEquals(3, prodById.size());
+	}
+
+	@Test
+	public void findItemTest() {
+		final int idToFind = 2;
+		Product p = Iterables.find(prods, new Predicate<Product>() {
+			public boolean apply(Product p) {
+				return p.getId() == idToFind;
+			}
+		});
+		Assert.assertEquals(Integer.valueOf(2), p.getId());
+	}
+
+	@Test
+	public void filteringItemsTest() {
+		final String catToFilter = "cat1";
+		Iterable<Product> filtered = Iterables.filter(prods,
+				new Predicate<Product>() {
+					public boolean apply(Product p) {
+						return catToFilter.equals(p.getCategory());
+					}
+				});
+		List<Product> list = Lists.newArrayList(filtered);
+		Assert.assertEquals(2, list.size());
+
+		Assert.assertFalse(Iterables.isEmpty(filtered));
+		;
+	}
+
+	@Test
+	public void groupItemsTest() {
+		ListMultimap<String, Product> prodsByCateg = Multimaps.index(prods,
+				new Function<Product, String>() {
+					public String apply(Product p) {
+						return p.getCategory();
+					}
+				});
+		List<Product> cat1Products = prodsByCateg.get("cat1");
+		Assert.assertEquals(2, cat1Products.size());
+	}
+
+	@Test
+	public void pagingTest() {
+		List<Integer> items = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+		List<List<Integer>> pages = Lists.partition(items, 3);
+		for (List<Integer> page : pages) {
+			System.out.println(page);// [1,2,3], [4,5,6], [7]
+		}
+		Assert.assertEquals(3, pages.size());
+	}
+
+	@Test
+	public void mapBuilderTest() {
+		Map<Integer, String> numbers = ImmutableMap.<Integer, String> builder()
+				.put(1, "one")
+				.put(2, "two")
+				.put(3, "three").build();
+		Assert.assertEquals(3, numbers.size());
+	}
+}
